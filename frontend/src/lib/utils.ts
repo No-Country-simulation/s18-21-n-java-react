59a449dd23003ebfbf489835dd68cf4275bce129
David-Yanez <luis.yanez@epn.edu.ec>
2024-10-09 14:10:32 -0500

    feat: add carousel

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,45 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+
+export const productList = [
+  {
+    image: '/images/alienware.jpg',
+    name: 'Auriculares Bluetooth',
+    description: 'Auriculares inalámbricos con excelente calidad de sonido.',
+    price: 50,
+  },
+  {
+    image: '/images/xiaomi.jpg',
+    name: 'Smartphone',
+    description: 'Smartphone con pantalla OLED y cámara de alta resolución.',
+    price: 800,
+  },
+  {
+    image: '/images/alienware.jpg',
+    name: 'Laptop Gamer',
+    description: 'Laptop con procesador potente y tarjeta gráfica de última generación.',
+    price: 1200,
+  },
+  {
+    image: '/images/xiaomi.jpg',
+    name: 'Mouse Inalámbrico',
+    description: 'Mouse inalámbrico ergonómico con gran precisión.',
+    price: 25,
+  },
+  {
+    image: '/images/alienware.jpg',
+    name: 'Smartwatch',
+    description: 'Reloj inteligente con monitoreo de salud y notificaciones.',
+    price: 150,
+  },
+  {
+    image: '/images/xiaomi.jpg',
+    name: 'Teclado Mecánico',
+    description: 'Teclado mecánico RGB con switches personalizables.',
+    price: 100,
+  },
+];
