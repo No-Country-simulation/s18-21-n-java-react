1cc6e52f36e7fab26f7a795cfcd5af80c73f831a
Dexametasona <machicadogomezalexander@gmail.com>
2024-10-17 22:11:41 -0500

    feat(cors): adding cors config

--- backend/e-commerce/src/main/java/com/dev/e_commerce/config/SecurityConfig.java
+++ backend/e-commerce/src/main/java/com/dev/e_commerce/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -30,6 +31,7 @@ public class SecurityConfig {
         return
                 http
                         .csrf(AbstractHttpConfigurer::disable)
+                        .cors(Customizer.withDefaults())
                         .authorizeHttpRequests(authRequest-> authRequest
                                 .requestMatchers("/swagger-ui/**",
                                         "/v3/api-docs/**").permitAll()
