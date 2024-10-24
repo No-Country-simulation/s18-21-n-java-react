d60e521843a15958f4d8da54c1c90402f8c19af1
Alejo <acolazurda@gmail.com>
2024-10-24 12:11:06 -0300

    styles problems fixed

--- frontend/src/components/Breadcrumb/Breadcrumb.tsx
+++ frontend/src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,23 +1,32 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@/components/ui/breadcrumb";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 export default function ShoppingCartBreadcrumb() {
   return (
-    <Breadcrumb className="mb-4 flex justify-center sm:justify-start">
-      <BreadcrumbItem>
-        <BreadcrumbLink href="#">Carrito</BreadcrumbLink>
-      </BreadcrumbItem>
-      <BreadcrumbItem>
-        <BreadcrumbLink href="#">Tus datos</BreadcrumbLink>
-      </BreadcrumbItem>
-      <BreadcrumbItem>
-        <BreadcrumbLink href="#">Pago</BreadcrumbLink>
-      </BreadcrumbItem>
-      <BreadcrumbItem>
-        {/* Enlace actual marcado con clase "font-bold" */}
-        <BreadcrumbLink href="#" className="font-bold text-gray-700">
-          Confirmar
-        </BreadcrumbLink>
-      </BreadcrumbItem>
-    </Breadcrumb>
+    <Breadcrumb>
+  <BreadcrumbList>
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/CartPage">carrito</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/products">tus datos</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+    <BreadcrumbItem>
+      <BreadcrumbLink href="/CartPage">pago</BreadcrumbLink>
+    </BreadcrumbItem>
+    <BreadcrumbSeparator />
+    <BreadcrumbItem>
+      <BreadcrumbPage>Confirmar</BreadcrumbPage>
+    </BreadcrumbItem>
+  </BreadcrumbList>
+</Breadcrumb>
   );
 }
