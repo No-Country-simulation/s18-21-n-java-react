36f464d74b80d17a0e454e15c30e28580c0313a2
Luis Delgado <dev.ledg555@gmail.com>
2024-10-29 16:48:53 -0600

    fix: remove extensions from components' barrel type

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,9 +1,9 @@
-export * from './header/Header.tsx';
-export * from './menu/Menu.tsx';
-export { default as ProductCard } from './product/ProductCard.tsx';
-export { default as ProductGrid } from './product/ProductGrid.tsx';
-export * from './product/ProductsCarousel.tsx';
-export * from './ui/button.tsx';
-export * from './ui/card.tsx';
-export * from './ui/carousel.tsx';
-export * from './ui/navigation-menu.tsx';
+export * from './header/Header';
+export * from './menu/Menu';
+export { default as ProductCard } from './product/ProductCard';
+export { default as ProductGrid } from './product/ProductGrid';
+export * from './product/ProductsCarousel';
+export * from './ui/button';
+export * from './ui/card';
+export * from './ui/carousel';
+export * from './ui/navigation-menu';
