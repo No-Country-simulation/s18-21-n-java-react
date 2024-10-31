faff366c69117a4dc282a5986db1834c579019f2
Luis Delgado <dev.ledg555@gmail.com>
2024-10-31 15:49:28 -0600

    fix: typos in home page

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -5,21 +5,14 @@ import { MainCarousel } from "@/components/homePage/MainCarousel";
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center">
-     
-          <MainCarousel/>
-       
-       <h2 className="text-4xl font-bold my-20">Lo mas vendido</h2>
-     
-        <ProductsCarousel />
-
-        <h2 className="text-4xl font-bold my-16 w-3/5 text-center">Contamos con los productos de las marcas mas reconocidas a nivel mundial </h2>
-        <InifiniteScroll/>
-     
-        <ProductGrid />
-
-      
-
-
+      <MainCarousel />
+      <h2 className="text-4xl font-bold my-20">Lo más vendido</h2>
+      <ProductsCarousel />
+      <h2 className="text-4xl font-bold my-16 w-3/5 text-center">
+        Contamos con los productos de las marcas más reconocidas a nivel mundial
+      </h2>
+      <InifiniteScroll />
+      <ProductGrid />
     </div>
   );
 }
